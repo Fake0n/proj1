83bb0eadcdd096b157277b88045707c0b34b1dc6
Sergey <97732361+Fake0n@users.noreply.github.com>
2023-02-02 00:36:23 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+try:
+    conn = sqlite3.connect('switches.db')
+    create_table = '''CREATE TABLE switches_base (
+                                switch_name TEXT NOT NULL,
+                                switch_ip INTEGER NOT NULL  UNIQUE,
+                                switch_mac INTEGER NOT NULL UNIQUE,
+                                switch_gateway INTEGER NOT NULL,
+                                ports_all INTEGER NOT NULL,
+                                ports_free INTEGER NOT NULL,
+                                port_mac INTEGER NOT NULL
+                                );'''
+    cur = conn.cursor()
+    print("База данных подключена к SQLite")
+    cur.execute(create_table)
+    conn.commit()
+    print("Таблица SQLite создана")
+
+    cur.close()
+
+except sqlite3.Error as error:
+    print("Ошибка при подключении к БД SQLite", error)
+
+finally:
+    if (conn):
+        conn.close()
+        print("Соединение c SQLite закрыто")
